cc0ff699164ea6863c267b73bb15d286aa2bef29
Piotr Warchołek <piotr.warcholek@student.pk.edu.pl>
2022-01-25 12:24:57 +0100

    Test

--- include/Taxes.h
+++ include/Taxes.h
@@ -9,22 +9,57 @@
 #include <string>
 #include "Help.h"
 using namespace std;
-
+/*
+ * class Taxees
+ */
 class Taxes {
-
+/*
+ * name - przechowuje nazwe klienta
+ * salary przechowuje wartosc wczytanej pensji
+ */
 string name;
 double salary;
 public:
-
+/*
+ * Kontener przechowywujący "dodatkowe zródlo zrodla dochodu"
+ */
     vector<double>income;
+    /*
+     * Konstruktor klasy Taxes
+     */
     Taxes();
+    /*
+     * Destruktor klasy Taxes
+     */
     ~Taxes();
+    /*
+     * Funkcja ustawiająca nazwe klienta
+     * name - nazwa klienta
+     */
     void set_name(string name);
+    /*
+     * Funkcja ustawiająca pensje klienta
+     * salary -zmienna przechowywujaca wartosc pensji
+     */
     void set_salary(double salary);
+    /*
+     * Funkcja zwracająca wartosc pensji
+     */
     double get_salary();
+    /*
+     * Funckja obliczająca podatek
+     * a - wartosc pensji
+     */
     double tex(double a);
+    /* Funckja obliczająca podatek  dla sumy dochodów obu osó
+     *
+     */
     double tex(double m, double n);
     string get_name();
+    /*
+     * Przeciążenie operatora +
+     * @parm Taxes &obj  przekazanie adresu obieku
+     */
     Taxes operator+(Taxes &obj)
     {
         Taxes wynik;
